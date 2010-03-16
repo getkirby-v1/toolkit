ee761727a2e3b2b493bb198c93a77f5fa0a9cfa9
Bastian Allgeier <bastian.allgeier@gmail.com>
2010-03-16 18:18:28 +0100

    added redirect headers to the go() function

--- kirby.php
+++ kirby.php
@@ -9,7 +9,21 @@ c::set('charset', 'utf-8');
 ############### HELPER ###############
 
 */
-function go($url) {
+function go($url, $code=false) {
+	// send an appropriate header
+	if($code) {
+		switch($code) {
+			case 301:
+				header('HTTP/1.1 301 Moved Permanently');
+				break;
+			case 302:
+				header('HTTP/1.1 302 Found');
+				break;
+			case 303:
+				header('HTTP/1.1 303 See Other');
+				break;
+		}
+	}
 	// send to new page
 	header('Location:' . $url);
 	exit();
