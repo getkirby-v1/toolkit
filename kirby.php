bb3fc02b097e4b7ceacca42d0039f6078b638a39
Bastian Allgeier <bastian.allgeier@gmail.com>
2010-03-29 12:16:11 +0200

    fixed a typo

--- kirby.php
+++ kirby.php
@@ -1073,9 +1073,9 @@ class l {
 			'de' => array('de_DE.UTF8','de_DE@euro','de_DE','de','ge'),
 			'fr' => array('fr_FR.UTF8','fr_FR','fr'),
 			'es' => array('es_ES.UTF8','es_ES','es'),
-			'it' => array('it_IT.UTF8','it_IT','it');
-			'pt' => array('pt_PT.UTF8','pt_PT','pt');
-			'zh' => array('zh_CN.UTF8','zh_CN','zh');
+			'it' => array('it_IT.UTF8','it_IT','it'),
+			'pt' => array('pt_PT.UTF8','pt_PT','pt'),
+			'zh' => array('zh_CN.UTF8','zh_CN','zh'),
 			'en' => array('en_US.UTF8','en_US','en'),
 		);
 		$locales = c::get('locales', array());
