a6089e04b0c28127649bccee086d267d3bca1e5c
Maxime Fabre <ehtnam6@gmail.com>
2012-03-16 02:25:17 +0100

    [add] f::type to determine the type of a file (kind of a reverse upload::mime_to_extension)

--- kirby.php
+++ kirby.php
@@ -2170,6 +2170,31 @@ class f {
   }
 
   /**
+   * Returns the type of a file according to its extension
+   * 
+   * @param string 	$file The file to analyze
+   * @return string The filetype
+   */
+  static function type($file) 
+  {
+  	if(str::find('.', $file)) $file = self::extension($file);
+	
+    $types = array(
+      'audio'     => array('aac', 'ac3', 'aif', 'aiff', 'm3a', 'm4a', 'm4b', 'mka', 'mp1', 'mp2', 'mp3', 'ogg', 'oga', 'ram', 'wav', 'wma'),
+      'video'     => array('asf', 'avi', 'divx', 'dv', 'flv', 'm4v', 'mkv', 'mov', 'mp4', 'mpeg', 'mpg', 'mpv', 'ogm', 'ogv', 'qt', 'rm', 'vob', 'wmv'),
+      'document'     => array('doc', 'docx', 'docm', 'dotm', 'odt', 'pages', 'pdf', 'rtf', 'wp', 'wpd'),
+      'spreadsheet'   => array('numbers', 'ods', 'xls', 'xlsx', 'xlsb', 'xlsm' ),
+      'interactive'   => array('key', 'ppt', 'pptx', 'pptm', 'odp', 'swf'),
+      'text'       => array('asc', 'csv', 'tsv', 'txt'),
+      'archive'     => array('bz2', 'cab', 'dmg', 'gz', 'rar', 'sea', 'sit', 'sqx', 'tar', 'tgz', 'zip'),
+      'code'       => array('css', 'htm', 'html', 'php', 'js'),
+      'image'      => array('jpeg', 'jpg', 'png', 'gif'));
+      
+    foreach($types as $type => $exts)
+      if(in_array($file, $exts)) return $type;
+  }
+
+  /**
    * Extracts the filename from a file path
    * 
    * @param  string  $file The path
